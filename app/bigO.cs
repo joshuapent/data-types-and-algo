935d945f4626564c231371c117e7b905408a3e4f
joshuapent <joshdavidpent@gmail.com>
2023-07-01 12:21:37 -0500

    C# caught up to JS + python

--- app/bigO.cs
+++ app/bigO.cs
@@ -6,8 +6,22 @@ namespace MyApp // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        static void findNemo(string[] array)
+        {
+          for (int i = 0; i < array.Length; i++)
+          {
+            Console.WriteLine(i);
+            if (array[i] == "nemo")
+            {
+              Console.WriteLine("Found", array[i]);
+            }
+          }
+        }
         static void Main(string[] args)
         {
+            string[] nemo = {"nemo"};
+            string[] everyone = {"dory", "bruce", "marlin", "nemo", "gill", "bloat", "nigel", "squirt", "darla", "hank"};
+            findNemo(everyone);
             Console.WriteLine("Hello World!");
         }
     }
