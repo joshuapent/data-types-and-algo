37bd7f152e4b73a116ac2c72d37d25944d0a9557
joshuapent <joshdavidpent@gmail.com>
2023-07-08 12:23:11 -0500

    Wrapping this up to work on C#

--- Problems/problem1.py
+++ Problems/problem1.py
@@ -3,13 +3,13 @@ array2 = [5, 6, 7, 8, 9]
 array3 = [1, 9, 10]
 
 def compArrays(arrayA, arrayB):
-    for num1 in arrayA:
-       for num2 in arrayB:
-           if num1 != num2:
-              break
-           else: 
-              return True
-    return False 
+  for num1 in arrayA:
+      for num2 in arrayB:
+          if num1 != num2:
+            break
+          else: 
+            return True
+  return False 
 
 result = compArrays(array1, array3)
 print(result)
