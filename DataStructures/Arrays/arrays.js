a7201b7abe170b70619a75c79997708253de47fd
joshuapent <joshdavidpent@gmail.com>
2023-08-30 12:12:15 -0500

    Did the same in python

--- DataStructures/Arrays/arrays.js
+++ DataStructures/Arrays/arrays.js
@@ -18,6 +18,17 @@ class Array {
   get(index) {
     return this.data[index]
   }
+  push(item) {
+    this.data[this.length] = item
+    this.length++
+    return self.length
+  }
+  pop() {
+    lastItem = this.data[this.length-1]
+    delete this.data[this.length-1]
+    this.length--
+    return lastItem
+  }
 }
 
 const newArray = new Array ();
