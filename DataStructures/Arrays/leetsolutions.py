5aa6b765091647b7bdb8c988104d67b5818c0a1c
joshuapent <joshdavidpent@gmail.com>
2023-09-19 09:27:34 -0500

    Setup for problem 1

---
+++ DataStructures/Arrays/leetsolutions.py
@@ -0,0 +1,5 @@
+def twoSum(nums, target):
+  
+nums = [1, 4, 9, 10]
+target = 5
+twoSum(nums, target)
---
+++ DataStructures/leetsolutions.js
@@ -0,0 +1,8 @@
+var twoSum = function(nums, target) {
+    
+};
+
+const nums = [1, 4, 9, 10];
+let target = 5;
+
+twoSum(nums, target);
